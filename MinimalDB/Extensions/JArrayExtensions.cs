3337ca4607b2912f940478bc0cace61f05e07888
rahiyansafin <rahiyansafin@gmail.com>
2024-07-08 11:35:28 +0600

    Fix GUID bug returning as string

--- MinimalDB/Extensions/JArrayExtensions.cs
+++ MinimalDB/Extensions/JArrayExtensions.cs
@@ -10,20 +10,17 @@ using Newtonsoft.Json.Linq;
 namespace MinimalDB.Extensions;
 public static class JArrayExtensions
 {
-    public static IDbCollection<T> ToDbCollection<T>(this JArray array)
+    public static ICollection<T> ToList<T>(this JArray array)
     {
-        DbCollection<T> list = [];
+        var list = new List<T>();
 
         foreach (var child in array.Children())
         {
             var json = child.ToString(Newtonsoft.Json.Formatting.None);
-
             var item = JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
             if (item != null)
                 list.Add(item);
         }
-
         return list;
     }
 }
--- MinimalDB/Program.cs
+++ MinimalDB/Program.cs
@@ -1,48 +1,57 @@
-﻿using MinimalDB;
-
-public record Employee(int Id, string Name, string Workplace)
+﻿namespace MinimalDB
 {
-    public List<string> strings => new List<string> { "Omo", "mo", "o" };
-}
-
-public record EmployeeV2(Guid Id, string Name, string Workplace);
-
-public class Test
-{
-    public static void Main(string[] args)
+    public record Employee(int Id, string Name, string Workplace)
     {
-        JsonStore v1 = new("database.json");
+        public static List<string> Strings => ["Omo", "mo", "o"];
+    }
 
-        //v1.Insert<Employee>(new Employee {Name = "Canice", Workplace = "Lafarge" });
+    public record EmployeeV2(Guid Id, string Name, string Workplace);
 
-        //v1.Commit();
-
-        var bb = v1.FindAll<Employee>();
-        foreach (var item in v1.FindAll<Employee>())
+    public class Test
+    {
+        public static void Main(string[] args)
         {
-            Console.WriteLine($"{item.Id} {item.Name} {item.Workplace}");
-        }
-        Console.ReadLine();
+            JsonStore v1 = new("database.json");
 
-        //Get me the employee with Id = 2
-        Console.WriteLine(v1.FindByCondition<Employee>(x => x.Name.Contains("cand", StringComparison.InvariantCultureIgnoreCase))?.FirstOrDefault()?.Name);
-        JsonStore v2 = new("v2.json");
-        try
-        {
-            //v2.Insert<EmployeeV2>(new EmployeeV2 {Name = "Raighne", Workplace = "Google" });
-            //v2.Insert<EmployeeV2>(new EmployeeV2 {Name = "Raighne", Workplace = "Google" });
-            //v2.Commit();
+            v1.Insert(new Employee(default, "Raighne", "Lafarge"));
+            v1.Insert(new Employee(24, "Onyeka", "Lafarge"));
+            v1.Insert(new Employee(default, "Canice", "Lafarge"));
 
-            foreach (var item in v2.FindAll<EmployeeV2>())
+            v1.Commit();
+
+            var bb = v1.FindAll<Employee>();
+            foreach (var item in v1.FindAll<Employee>())
             {
                 Console.WriteLine($"{item.Id} {item.Name} {item.Workplace}");
             }
             Console.ReadLine();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message.ToString());
-            Console.ReadLine();
+
+            // Get me the employee with Id = 2
+            Console.WriteLine(v1.FindByCondition<Employee>(x => x.Name.Contains("cani", StringComparison.InvariantCultureIgnoreCase))?.FirstOrDefault()?.Name);
+            //JsonStore v2 = new JsonStore("v2.json");
+            //try
+            //{
+            //    //v2.Insert<EmployeeV2>(new EmployeeV2 { Id = Guid.NewGuid(), Name = "Onyeka", Workplace = "Google" });
+            //    v2.Insert<EmployeeV2>(new EmployeeV2 { Name = "Raighne", Workplace = "Google" });
+            //    v2.Insert<EmployeeV2>(new EmployeeV2 { Id = Guid.NewGuid(), Name = "Onyeka", Workplace = "Google" });
+
+            //    v2.Commit();
+
+            //    foreach (var item in v2.FindAll<EmployeeV2>())
+            //    {
+            //        Console.WriteLine($"{item.Id} {item.Name} {item.Workplace}");
+            //    }
+
+            //    Console.WriteLine(v2.FindByCondition<EmployeeV2>(x => x.Id == Guid.Parse("68b6705e-960d-40c4-9182-f430afa55e70"))
+            //        .FirstOrDefault()
+            //        .Name);
+            //    Console.ReadLine();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message.ToString());
+            //    Console.ReadLine();
+            //}
         }
     }
 }
